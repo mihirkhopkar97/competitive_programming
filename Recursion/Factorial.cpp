04e87593bd1d327507616bc41766825190929166
Mihir Khopkar <mihirkhopkar97@gmail.com>
2024-03-13 22:42:53 +0530

    Technical change log - factorial using recursion

---
+++ Recursion/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int factorial(int x){
+    if(x==0){
+        return 1;
+    }else{
+        return (x*factorial(x-1));
+    }
+}
+int main(){
+    int x,ans;
+    cin>>x;
+    ans=factorial(x);
+    cout<<ans;
+    return 0;
+}
